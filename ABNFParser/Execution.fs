e81020c3485cadbc592e720e0c33e447003600c0
FatherFoxxy <rattusunison@gmail.com>
2019-06-21 21:57:44 -0700

    Add Repetition/Sequence execution

--- ABNFParser/Execution.fs
+++ ABNFParser/Execution.fs
@@ -2,6 +2,7 @@ module ABNF.Execution
 open System
 open FParsec
 open System.Data
+open System
 
 let parse (text : string) =
     run pDocument text
@@ -18,20 +19,78 @@ let rec matchElements (rules : ABNFRule list) (str : RuleStream) (elements : Rul
         | Terminals        terminals ->
             terminals
             |> List.fold (fun ((isMatch, rs) : bool * RuleStream) (terminal : Terminal) ->
-                if isMatch && rs.Peek() = terminal then
+                if isMatch && rs.Peek() = Next terminal then
                     (true, { rs with Pos = rs.Pos + 1 })
                 else
                     (false, rs)) (true, str)
         | Alternatives     elements ->
             (true, str)
         | OptionalSequence element ->
-            (true, str)
+            let result = matchElement str element
+            if fst result then
+                result
+            else
+                (true, str)
         | Group            elements ->
             (true, str)
         | Sequence         elements ->
-            (true, str)
+            elements
+            |> List.fold (fun (isMatch, stream) element ->
+                if isMatch then
+                    matchElement stream element
+                else
+                    (false, str)) (true, str)
+        // I know, I know. Imperative BS. But, I'm lazy and it was easier this way ¯\_(ツ)_/¯
         | Repetition       (range, element) ->
-            (true, str)
+            let mutable count = 0uy
+            let mutable temp = (true, str)
+            match range with
+            | Any ->
+                while fst temp do
+                    temp <- matchElements rules (snd temp) [element]
+                (true, snd temp)
+            | AtLeast (atLeast) ->
+                while fst temp do
+                    temp <- matchElements rules (snd temp) [element]
+                    if fst temp then
+                        count <- count + 1uy
+                if count >= atLeast then
+                    (true, snd temp)
+                else
+                    (false, str)
+            | AtMost (atMost) ->
+                while fst temp && count + 1uy <= atMost do
+                    temp <- matchElements rules (snd temp) [element]
+                    if fst temp then
+                        count <- count + 1uy
+                if count <= atMost then
+                    (true, snd temp)
+                else
+                    (false, str)
+            | Exactly (exactly) ->
+                let mutable shortCircuit = false
+                while fst temp && not shortCircuit do
+                    temp <- matchElements rules (snd temp) [element]
+                    if fst temp then
+                        count <- count + 1uy
+                        if count = exactly then
+                            shortCircuit <- true
+                if count = exactly then
+                    (true, snd temp)
+                else
+                    (false, str)
+            | Between (min, max) ->
+                let mutable shortCircuit = false
+                while fst temp && not shortCircuit do
+                    temp <- matchElements rules (snd temp) [element]
+                    if fst temp then
+                        count <- count + 1uy
+                        if count = max then
+                            shortCircuit <- true
+                if count >= min && count <= max then
+                    (true, snd temp)
+                else
+                    (false, str)
         | RuleReference    string ->
             (true, str)
     matchElement str elements.[0]
