1258330be94ae97426b71254639060ffdcc86e69
FatherFoxxy <rattusunison@gmail.com>
2020-04-18 14:25:46 -0700

    Remove explicit namespace, short circuit terminal matching on EOF, move function to Grammar

--- Parser/Execution.fs
+++ Parser/Execution.fs
@@ -1,9 +1,5 @@
-module ABNF.Execution
+module Execution
 open FParsec
-
-let parseAllRules (text : string) =
-    runParserOnString pDocument [ ] "" text
-
 let findRule (rules : Rule list) name =
     let definitions =
         rules
@@ -23,105 +19,105 @@ let findRule (rules : Rule list) name =
 
 let rec matchElements (rules : Rule list) (str : RuleStream) (elements : RuleElement list) =
     let rec matchElement (str : RuleStream) (element : RuleElement) =
-        if (str.Pos = str.Text.Length) then
-            printfn "Unexpected end of stream, could not process %O" element
-            (false, str)
-        else
-            printfn "%s" (str.Text.Replace("\r", "\\r").Replace("\n", "\\n"))
-            for z = 0 to str.Pos - 1 do
-                printf "-"
-            printfn "^"
-            printfn "%s\r\n" (element.ToString())
-            match element with
-            | Terminals        terminals ->
+        let isEndOfStream = str.Pos = str.Text.Length
+        printfn "%s" (str.Text.Replace("\r", "\\r").Replace("\n", "\\n"))
+        for z = 0 to str.Pos - 1 do
+            printf "-"
+        printfn "^"
+        printfn "%s\r\n" (element.ToString())
+        match element with
+        | Terminals        terminals ->
+            if isEndOfStream then
+                (false, str)
+            else
                 terminals
                 |> List.fold (fun ((isMatch, rs) : bool * RuleStream) (terminal : Terminal) ->
                     if isMatch && rs.Peek() = Next terminal then
                         (true, { rs with Pos = rs.Pos + 1 })
                     else
                         (false, rs)) (true, str)
-            | Alternatives     elements ->
-                let result =
-                    elements
-                    |> List.tryPick (fun e ->
-                        let result = matchElement str e
-                        if fst result then
-                            Some result
-                        else
-                            None)
-                match result with
-                | Some res -> res
-                | None -> (false, str)
-            | OptionalSequence element ->
-                let result = matchElement str element
-                if fst result then
-                    result
-                else
-                    (true, str)
-            | Sequence         elements ->
+        | Alternatives     elements ->
+            let result =
                 elements
-                |> List.fold (fun (isMatch, stream) element ->
-                    if isMatch then
-                        matchElement stream element
+                |> List.tryPick (fun e ->
+                    let result = matchElement str e
+                    if fst result then
+                        Some result
                     else
-                        (false, str)) (true, str)
-            // I know, I know. Imperative BS. But, I'm lazy and it was easier this way ¯\_(ツ)_/¯
-            | Repetition       (range, element) ->
-                let mutable count = 0uy
-                let mutable temp = (true, str)
-                match range with
-                | Any ->
-                    while fst temp do
-                        temp <- matchElement (snd temp) element
+                        None)
+            match result with
+            | Some res -> res
+            | None -> (false, str)
+        | OptionalSequence element ->
+            let result = matchElement str element
+            if fst result then
+                result
+            else
+                (true, str)
+        | Sequence         elements ->
+            elements
+            |> List.fold (fun (isMatch, stream) element ->
+                if isMatch then
+                    matchElement stream element
+                else
+                    (false, str)) (true, str)
+        // I know, I know. Imperative BS. But, I'm lazy and it was easier this way ¯\_(ツ)_/¯
+        | Repetition       (range, element) ->
+            let mutable count = 0uy
+            let mutable temp = (true, str)
+            match range with
+            | Any ->
+                while fst temp do
+                    temp <- matchElement (snd temp) element
+                (true, snd temp)
+            | AtLeast (atLeast) ->
+                while fst temp do
+                    temp <- matchElement (snd temp) element
+                    if fst temp then
+                        count <- count + 1uy
+                if count >= atLeast then
                     (true, snd temp)
-                | AtLeast (atLeast) ->
-                    while fst temp do
-                        temp <- matchElement (snd temp) element
-                        if fst temp then
-                            count <- count + 1uy
-                    if count >= atLeast then
-                        (true, snd temp)
-                    else
-                        (false, str)
-                | AtMost (atMost) ->
-                    while fst temp && count + 1uy <= atMost do
-                        temp <- matchElement (snd temp) element
-                        if fst temp then
-                            count <- count + 1uy
-                    if count <= atMost then
-                        (true, snd temp)
-                    else
-                        (false, str)
-                | Exactly (exactly) ->
-                    let mutable shortCircuit = false
-                    while fst temp && not shortCircuit do
-                        temp <- matchElement (snd temp) element
-                        if fst temp then
-                            count <- count + 1uy
-                            if count = exactly then
-                                shortCircuit <- true
-                    if count = exactly then
-                        (true, snd temp)
-                    else
-                        (false, str)
-                | Between (min, max) ->
-                    let mutable shortCircuit = false
-                    while fst temp && not shortCircuit do
-                        temp <- matchElement (snd temp) element
-                        if fst temp then
-                            count <- count + 1uy
-                            if count = max then
-                                shortCircuit <- true
-                    if count >= min && count <= max then
-                        (true, snd temp)
-                    else
-                        (false, str)
-            | RuleReference    string ->
-                let rule =
-                    rules
-                    |> List.find (fun rule ->
-                        rule.RuleName.ToUpper() = string.ToUpper())
-                matchElements rules str rule.Definition
+                else
+                    (false, str)
+            | AtMost (atMost) ->
+                while fst temp && count + 1uy <= atMost do
+                    temp <- matchElement (snd temp) element
+                    if fst temp then
+                        count <- count + 1uy
+                if count <= atMost then
+                    (true, snd temp)
+                else
+                    (false, str)
+            | Exactly (exactly) ->
+                let mutable shortCircuit = false
+                while fst temp && not shortCircuit do
+                    temp <- matchElement (snd temp) element
+                    if fst temp then
+                        count <- count + 1uy
+                        if count = exactly then
+                            shortCircuit <- true
+                if count = exactly then
+                    (true, snd temp)
+                else
+                    (false, str)
+            | Between (min, max) ->
+                let mutable shortCircuit = false
+                while fst temp && not shortCircuit do
+                    temp <- matchElement (snd temp) element
+                    if fst temp then
+                        count <- count + 1uy
+                        if count = max then
+                            shortCircuit <- true
+                if count >= min && count <= max then
+                    (true, snd temp)
+                else
+                    (false, str)
+        | RuleReference    string ->
+            let rule =
+                rules
+                |> List.find (fun rule ->
+                    rule.RuleName.ToUpper() = string.ToUpper())
+            matchElements rules str rule.Definition
 
     let success, _, _, rs =
         elements
