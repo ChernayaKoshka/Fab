cb55308494899fea1a194c9b72980f931718fbc9
FatherFoxxy <rattusunison@gmail.com>
2020-04-18 14:27:11 -0700

    Simplify test runner

--- Tests/Main.fs
+++ Tests/Main.fs
@@ -3,10 +3,4 @@ open System
 open Tests
 
 [<EntryPoint>]
-let main argv =
-    let res = runTestsInAssembly defaultConfig argv
-    // let res = runTests { defaultConfig with runInParallel = false } test2
-    if res <> 0 then
-        Console.ReadLine()
-        |> ignore
-    res
+let main argv = runTestsInAssembly defaultConfig argv
