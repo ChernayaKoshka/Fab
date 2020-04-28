9cbd021830f30aa4cb1ac2b6722f2ae6db316498
FatherFoxxy <rattusunison@gmail.com>
2020-04-27 22:15:25 -0700

    Add additional TypeProvider examples

--- Fab/ABNFRegexProvider/Example/example.fsx
+++ Fab/ABNFRegexProvider/Example/example.fsx
@@ -1,7 +1,7 @@
 #r @"..\bin\Debug\netstandard2.0\Fab.ABNFProvider.dll"
 open Fab.ABNFRegexProvider
 
-type PostalAddress = ABNFRegex< @"Fab\ABNFRegexProvider\Example\test.abnf" >
+type PostalAddress = ABNFRegex< @"Fab\ABNFRegexProvider\Example\postal-address.abnf" >
 
 let address =
     "Test lastnamme Sr.\r\n123 Main St\r\nFakeTown, AA 12345\r\n"
@@ -9,3 +9,9 @@ let address =
 let postalAdressCap = PostalAddress.``postal-address``.Match(address)
 
 printfn "%b|%s" postalAdressCap.Success postalAdressCap.Groups.["street"].Value
+
+type IRCMessage = ABNFRegex< @"Fab\ABNFRegexProvider\Example\irc.abnf" >
+
+let testMessage = ":WiZ!jto@tolsun.oulu.fi NICK Kilroy\r\n"
+
+IRCMessage.message.Match(testMessage)
